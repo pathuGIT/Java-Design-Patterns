f3ffa1afb3404879de13ad0e62f97966cfd8f3a3
pathuGIT <wannigelakshan@gmail.com>
2025-11-14 02:19:54 +0530

    structure changed

--- . RMI/RMIChatAi/src/ChatService.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.List;
-
-public interface ChatService extends Remote {
-    void sendMessage(String username, String message) throws RemoteException;
-    List<String> getMessages() throws RemoteException;
-}
